890e5d2adf7c97bf9aa78cbf8aac46bb81d9a289
saunved <saunved@gmail.com>
2023-02-18 17:10:11 +0530

    Setup Layouts and translation strings

---
+++ frontend/src/constants/ui/strings.js
@@ -0,0 +1,61 @@
+export default {
+  EN: {
+    SITE: {
+      woofer: "Woofer",
+      home: "Home",
+    },
+    LOGIN_FLOW: {
+      signInCta: "Log in to Woofer",
+      email: "Email or username",
+      password: "Enter your password",
+      birthday: "Enter your date of birth",
+      forgotPassword: "Forgot password?",
+      signIn: "Log in",
+      newUser: "Don't have an account?",
+      register: "Register",
+    },
+    REGISTER_FLOW: {
+      registerCta: "Register for Woofer",
+      email: "Your email",
+      password: "Create a password",
+      register: "Register",
+      birthday: "Enter your date of birth",
+      forgotPassword: "Forgot password?",
+      existingUser: "Already have an account?",
+      signIn: "Log in",
+      date: "Your date of birth",
+      name: "Your display name",
+      handle: "Create a handle",
+    },
+    LOGOUT: {
+      loggingYouOut: "Logging you out...",
+    },
+    SETTINGS: {
+      settings: "Settings",
+      changePassword: "Change password",
+      currentPassword: "Current password",
+      newPassword: "New password",
+      newPasswordAgain: "New password again",
+      changePassword: "Change password",
+      accountDetails: "Account details",
+      logout: "Log out",
+      yourBio: "Your bio",
+      website: "Website",
+      update: "Update",
+    },
+    FOLLOW: {
+      followers: "Followers",
+      following: "Following",
+      follow: "Follow",
+      unfollow: "Unfollow",
+    },
+    FEED: {
+      following: "Following",
+      forYou: "For you",
+    },
+    PROFILE: {
+      joined: "Joined",
+      woofs: "Woofs",
+    },
+  },
+};
