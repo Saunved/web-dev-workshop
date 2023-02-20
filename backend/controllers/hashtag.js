a50b2aeb43dac81b3303aa45fab8c936673f02e7
megh-khaire <meghkhaire@gmail.com>
2023-02-21 02:54:16 +0530

    Cleanup controller for hashtags

--- backend/controllers/hashtag.js
+++ backend/controllers/hashtag.js
@@ -2,7 +2,6 @@ const Hashtag = require("./../models/Hashtag");
 
 module.exports.createHashtag = async (req, res) => {
   try {
-    // Add tag to Hashtag model
     const hashtag = await Hashtag.create(req.body);
 
     return res.status(201).json({
@@ -16,26 +15,12 @@ module.exports.createHashtag = async (req, res) => {
   }
 };
 
-module.exports.getHashtag = async (req, res) => {
-  try {
-    const hashtag = await Hashtag.findOne({ where: { id: req.query.id } });
-
-    return res.status(200).json({
-      data: { hashtag: hashtag },
-    });
-  } catch (err) {
-    return res.status(500).json({
-      message: "Error while fetching hashtag.",
-    });
-  }
-};
-
 module.exports.getAllHashtags = async (req, res) => {
   try {
-    const hashtag = await Hashtag.findAll({});
+    const hashtags = await Hashtag.findAll({});
 
     return res.status(200).json({
-      date: { hashtags: hashtag },
+      data: { hashtags: hashtags },
     });
   } catch (err) {
     return res.status(500).json({
