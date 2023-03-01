ef59f76f33ff3fbd3c8eb416288f17f7dc0867e1
rishabhj1717 <rishabhj1717@gmail.com>
2023-03-01 22:56:48 +0530

    Update follows user api
    - Update follower count and following count for the repsective user ids
    - Add checks and update only if both user ids exist in our system

--- backend/controllers/follows.js
+++ backend/controllers/follows.js
@@ -1,13 +1,45 @@
 const Follows = require("./../models/Follows");
+const User = require("./../models/User");
 
 module.exports.createFollows = async (req, res) => {
   try {
+
     const followingUserId = req.params.followingUserId;
     const userId = req.user.id;
-    const follows = await Follows.create({ userId, followingUserId });
+
+    if (userId == followingUserId) {
+      return res.status(400).json({
+        message: "Users cannot folow themselves",
+      });
+    }
+
+    const [follows, created] = await Follows.findOrCreate({
+      where: { userId, followingUserId },
+      defaults: { userId, followingUserId }
+    });
+
+    if (!created) {
+      return res.status(200).json({
+        message: "The users are already following each other"
+      });
+    }
+
+    //increment following count for user
+    const user = await User.findByPk(userId);
+
+    //increment follower count for user
+    const followedUser = await User.findByPk(followingUserId);
+
+    if (!user || !followedUser) {
+      return res.status(400).json({
+        message: "Incorrect user ids",
+      });
+    }
+
+    await user.increment('followingCount');
+    await followedUser.increment('followerCount');
 
     return res.status(201).json({
-      data: { follows: { id: follows.id } },
       message: "Follows created."
     });
   } catch (err) {
