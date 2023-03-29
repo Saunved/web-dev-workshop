bffd50cb296f69fca629562e7e7e0ca4d17200e3
saunved <saunved@gmail.com>
2023-03-30 02:45:25 +0530

    Consume the get single tweet API

--- frontend/src/pages/[profile]/status/[id].jsx
+++ frontend/src/pages/[profile]/status/[id].jsx
@@ -1,6 +1,49 @@
 import Tweet from "@/components/Tweet/SingleTweet";
-import tweets from "@/mock/tweets";
+import { BASE_URL } from "@/constants/routes";
+import strings from "@/constants/ui/strings";
 
-export default function SingleUserTweet() {
-  return <Tweet tweet={tweets[0]} />;
+export default function SingleUserTweet({ tweet }) {
+  const uiText = strings.EN.TWEET;
+
+  if (!tweet) {
+    return <p className="min-h-screen h-full">{uiText.tweetNotFound}</p>;
+  }
+
+  return <Tweet tweet={tweet} />;
+}
+
+export async function getServerSideProps({ params }) {
+  if (!params.id) {
+    return {
+      props: {
+        tweet: null,
+      },
+    };
+  }
+  if (params.id) {
+    try {
+      const res = await fetch(`${BASE_URL}/tweet/${params.id}`);
+      const body = await res.json();
+
+      if (res.ok) {
+        return {
+          props: {
+            tweet: body.data.tweet,
+          },
+        };
+      } else {
+        return {
+          props: {
+            tweet: null,
+          },
+        };
+      }
+    } catch (error) {
+      return {
+        props: {
+          tweet: null,
+        },
+      };
+    }
+  }
 }
