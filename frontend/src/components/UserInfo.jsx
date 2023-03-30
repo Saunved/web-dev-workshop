b54b8470deb750af532e6c8bfb2f92fd072abdc5
saunved <saunved@gmail.com>
2023-03-30 20:40:13 +0530

    Link up user profile info to user profile page

--- frontend/src/components/UserInfo.jsx
+++ frontend/src/components/UserInfo.jsx
@@ -1,5 +1,6 @@
 import ProfilePicture from "./Tweet/ProfilePicture";
 import strings from "@/constants/ui/strings";
+import Link from "next/link";
 
 export default function UserInfo({ user }) {
   const uiText = strings.EN.FOLLOW;
@@ -7,14 +8,16 @@ export default function UserInfo({ user }) {
   return (
     <div className="flex w-full gap-4 px-4">
       <div className="w-12">
-        <ProfilePicture handle={user.handle} />
+        <Link href={`/${user.handle}`}>
+          <ProfilePicture handle={user.handle} />
+        </Link>
       </div>
       <div className="grow">
         <div className="w-full flex justify-between items-center">
-          <div>
+          <Link href={`/${user.handle}`}>
             <div className="font-semibold">{user.name}</div>
             <div className="text-sm text-gray-700">@{user.handle}</div>
-          </div>
+          </Link>
           <button className="border border-gray-600 rounded-full px-8 py-0.5 text-sm">
             {uiText.follow}
           </button>
