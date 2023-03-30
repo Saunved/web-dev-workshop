cfa5ddd9b6f234b4e93d8762ee43eb8d41cb1a72
saunved <saunved@gmail.com>
2023-03-30 19:44:30 +0530

    Show a message if there are no tweets to show

--- frontend/src/components/TweetFeed.jsx
+++ frontend/src/components/TweetFeed.jsx
@@ -1,6 +1,10 @@
 import Tweet from "@/components/Tweet/SingleTweet";
 
 export default function TweetFeed({ tweets }) {
+  if (!tweets.length) {
+    return <div className="text-center my-8">No tweets to show right now!</div>;
+  }
+
   return (
     <>
       {tweets.map((tweet, idx) => {
