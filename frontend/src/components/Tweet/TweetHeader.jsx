593974645654886b66c2db30363092a360380111
saunved <saunved@gmail.com>
2023-03-30 20:49:14 +0530

    Add @ before handle

--- frontend/src/components/Tweet/TweetHeader.jsx
+++ frontend/src/components/Tweet/TweetHeader.jsx
@@ -2,7 +2,7 @@ export default function TweetHeader({ name, handle, time }) {
   return (
     <div className="grid grid-flow-col gap-1 justify-start">
       <p className="font-bold">{name}</p>
-      <p className="text-gray-700">{handle}</p>
+      <p className="text-gray-700">@{handle}</p>
       <p>&bull;</p>
       <p>{time}</p>
     </div>
