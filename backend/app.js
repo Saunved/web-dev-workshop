dc75011493c441363951fb10208ef10ce927f9c7
megh-khaire <meghkhaire@gmail.com>
2023-03-30 23:14:26 +0530

    Allow PUT and DELETE methods in CORS implementation

--- backend/app.js
+++ backend/app.js
@@ -32,6 +32,7 @@ app.use((req, res, next) => {
   }
   res.setHeader("Access-Control-Allow-Credentials", "true");
   res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
   next();
 });
 
