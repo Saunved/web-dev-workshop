6347e1960cfb0c0a44fbf683be7ac8bb454cdd23
rish17 <rishabhj1717@gmail.com>
2023-04-05 21:53:27 +0530

    Remove frontend automation

--- frontend/src/constants/routes.js
+++ frontend/src/constants/routes.js
@@ -1,4 +1,4 @@
-export const BASE_URL = process.env.USE_CODESPACE == 'yes' ? `https://${process.env.CODESPACE_NAME}-5000.preview.app.github.dev` : "http://localhost:9800";
+export const BASE_URL = "http://localhost:9800";
 
 export const loginRoute = `${BASE_URL}/login`;
 export const registerRoute = `${BASE_URL}/user`;
