d04557e105eab840707e8779c5a494b6516f956a
sumeet14 <sumeetkoli4@gmail.com>
2023-04-07 20:19:02 +0530

    Third checkpoint

---
+++ save-points/3/frontend/src/pages/[profile]/followers.jsx
@@ -0,0 +1,49 @@
+import UserList from "@/components/UserList";
+import strings from "@/constants/ui/strings";
+import { BASE_URL } from "@/constants/routes";
+import { attachAuthCookie } from "@/utils/xhr";
+
+export default function FollowersPage({ users = [] }) {
+  const uiText = strings.EN.FOLLOW;
+  return (
+    <div className="min-h-screen">
+      <div>
+        <h1 className="text-2xl font-bold px-4 mt-2">{uiText.followers}</h1>
+      </div>
+      {!users.length ? (
+        <section className="text-center my-8">
+          Hmm, looks like no one is following this user yet
+        </section>
+      ) : (
+        <section className="mt-6">
+          <UserList users={users} />
+        </section>
+      )}
+    </div>
+  );
+}
+
+export async function getServerSideProps({ req, res, params }) {
+  try {
+    if (params.profile) {
+      const followingRes = await fetch(
+        `${BASE_URL}/followers/${params.profile}`,
+        attachAuthCookie(req)
+      );
+      const followingResBody = await followingRes.json();
+
+      return {
+        props: {
+          users: followingResBody.data.followers,
+        },
+      };
+    }
+  } catch (error) {
+    console.log(error);
+    return {
+      props: {
+        users: [],
+      },
+    };
+  }
+}
