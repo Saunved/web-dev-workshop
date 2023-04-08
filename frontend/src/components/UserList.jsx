520226f2089ca40f7af19bcee7741268c8f1561c
Saunved Mutalik <saunved@gmail.com>
2023-04-08 13:49:44 +0000

    Use user ID instead of idx

--- frontend/src/components/UserList.jsx
+++ frontend/src/components/UserList.jsx
@@ -4,8 +4,8 @@ export default function UserList({ users }) {
   return (
     <>
       <div className="grid gap-8">
-        {users.map((user, idx) => {
-          return <UserInfo user={user} key={idx} />;
+        {users.map((user) => {
+          return <UserInfo user={user} key={user.id} />;
         })}
       </div>
     </>
