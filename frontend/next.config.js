01e8232bed08d6674888c5ec6e3306f89a5c04c3
saunved <saunved@gmail.com>
2023-04-09 13:24:47 +0530

    Set the prod url for demo

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -2,8 +2,8 @@
 const nextConfig = {
   reactStrictMode: true,
   env: {
-    BASE_URL: process?.env?.CODESPACE_NAME ? `https://${process.env.CODESPACE_NAME}-5000.preview.app.github.dev` : 'http://localhost:5000'
-  }
-}
+    BASE_URL: "https://sbox-woofer.helpshift.mobi",
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
