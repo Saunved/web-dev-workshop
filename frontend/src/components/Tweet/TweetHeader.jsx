d6df4463c7c95e3a88ed74ad38bc1671ea55ef33
saunved <saunved@gmail.com>
2023-04-13 16:57:12 +0530

    Make single tweet header info responsive

--- frontend/src/components/Tweet/TweetHeader.jsx
+++ frontend/src/components/Tweet/TweetHeader.jsx
@@ -1,10 +1,16 @@
 export default function TweetHeader({ name, handle, time }) {
   return (
-    <div className="grid grid-flow-col gap-1 justify-start">
-      <p className="font-bold">{name}</p>
-      <p className="text-gray-700">@{handle}</p>
-      <p>&bull;</p>
-      <p>{time}</p>
+    <div className="grid grid-flow-col gap-1 justify-start text-sm md:text-base">
+      <p className="font-bold max-w-xs overflow-hidden whitespace-nowrap text-ellipsis">
+        {name}
+      </p>
+      <p className="text-gray-700 max-w-xs overflow-hidden whitespace-nowrap text-ellipsis">
+        @{handle}
+      </p>
+      <p className="">&bull;</p>
+      <p className="max-w-xs overflow-hidden whitespace-nowrap text-ellipsis">
+        {time}
+      </p>
     </div>
   );
 }
